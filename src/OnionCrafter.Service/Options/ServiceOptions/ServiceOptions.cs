5a504f4ef8d42db554d0dd41c42604a82e395d3a
Dtopiast <dtopiast@protonmail.com>
2023-05-25 11:44:26 -0600

    Reimplementation of ServiceOptions

---
+++ src/OnionCrafter.Service/Options/ServiceOptions/ServiceOptions.cs
@@ -0,0 +1,24 @@
+﻿using OnionCrafter.Service.Options.GlobalOptions;
+
+namespace OnionCrafter.Service.Options.ServiceOptions
+{
+    /// <summary>
+    /// Represents the options for a service that can be configured using a generic type.
+    /// </summary>
+    public class ServiceOptions<TGlobalOptions> : IServiceOptions
+    {
+        /// <inheritdoc/>
+        public string? SetServiceName { get; set; }
+
+        /// <inheritdoc/>
+
+        public bool UseLogger { get; set; }
+    }
+    /// <summary>
+    /// Represents the options for a service.
+    /// </summary>
+    public class ServiceOptions : ServiceOptions<GlobalOptions.GlobalOptions>
+    {
+
+    }
+}
