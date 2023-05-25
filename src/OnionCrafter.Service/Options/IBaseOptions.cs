daeb2e50231e52cdc595855e728f9321d15f6d3f
Dtopiast <dtopiast@protonmail.com>
2023-05-25 11:48:32 -0600

    Creation of the IBaseOptions interface to have a low level of inheritance for all option types

--- src/OnionCrafter.Service/Options/IBaseOptions.cs
+++ src/OnionCrafter.Service/Options/IBaseOptions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OnionCrafter.Service.Options.ServiceOptions
+namespace OnionCrafter.Service.Options
 {
     /// <summary>
     /// Interface for providing base options.
