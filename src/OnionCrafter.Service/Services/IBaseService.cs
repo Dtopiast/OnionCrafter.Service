8379972b7f3b9008260bc51cb6e8f965a1ca738c
dtopiast <dtopiast@protonmail.com>
2023-06-19 13:37:24 -0600

    Update of IBaseService and IService

--- src/OnionCrafter.Service/Services/IBaseService.cs
+++ src/OnionCrafter.Service/Services/IBaseService.cs
@@ -1,11 +1,20 @@
 ﻿using OnionCrafter.Base.Commons;
+using OnionCrafter.Service.Options.GlobalOptions;
+using OnionCrafter.Service.Options.ServiceOptions;
 
 namespace OnionCrafter.Service.Services
 {
     /// <summary>
-    /// Base interface for a service.
+    /// Interface for a base service that implements IAsyncDisposable and IPrototype.
     /// </summary>
-    public interface IBaseService : IAsyncDisposable, IPrototype<IService>
+    public interface IBaseService : IPrototype<IBaseService>
+    {
+    }
+
+    /// <summary>
+    /// Represents an interface for a generic base service with options.
+    /// </summary>
+    public interface IBaseService<TBaseServiceOptions> : IBaseService
     {
     }
 }
--- src/OnionCrafter.Service/Services/IService.cs
+++ src/OnionCrafter.Service/Services/IService.cs
@@ -1,4 +1,6 @@
-﻿using OnionCrafter.Service.Services.Options;
+﻿using OnionCrafter.Base.Commons;
+using OnionCrafter.Service.Options.GlobalOptions;
+using OnionCrafter.Service.Options.ServiceOptions;
 
 namespace OnionCrafter.Service.Services
 {
@@ -11,32 +13,13 @@ namespace OnionCrafter.Service.Services
         /// Gets the name of the service.
         /// </summary>
         string Name { get; }
-
-        /// <summary>
-        /// Logs an action with the specified result, action type, and arguments.
-        /// </summary>
-        /// <typeparam name="TActions">The type of the action.</typeparam>
-        /// <param name="actionResult">The result of the action.</param>
-        /// <param name="action">The action type.</param>
-        /// <param name="args">The arguments for the action.</param>
-        /// <remarks>
-        /// This method logs the specified action with its result and additional arguments.
-        /// The action type must be an enum.
-        /// </remarks>
-        void LogAction<TActions>(bool actionResult, TActions action, params object?[] args)
-            where TActions : struct, Enum;
     }
 
     /// <summary>
-    /// Interface for a service with specific options.
+    /// Represents an interface for a generic service with options of type TServiceOptions.
     /// </summary>
-    /// <typeparam name="TServiceOptions">The type of options for the service.</typeparam>
-    public interface IService<TServiceOptions> : IService
-        where TServiceOptions : IServiceOptions
+    public interface IService<TServiceOptions> : IService, IBaseService<TServiceOptions>
+           where TServiceOptions : class, IBaseServiceOptions
     {
-        /// <summary>
-        /// Gets the configuration options for the service.
-        /// </summary>
-        public TServiceOptions Config { get; }
     }
 }
