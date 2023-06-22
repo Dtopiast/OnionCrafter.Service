536a8464cb6f0d835fb92e4f95e2d914c6777dd6
dtopiast <dtopiast@protonmail.com>
2023-06-22 01:53:13 -0600

    Creation of Logging

---
+++ src/OnionCrafter.Service/Logging/ILogAction.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace OnionCrafter.Service.Logging
+{
+    /// <summary>
+    /// Interface representing a log action.
+    /// </summary>
+    public interface ILogAction : IBaseLogAction
+    {
+        /// <summary>
+        /// Gets or sets the success message for the log action.
+        /// </summary>
+        string SuccessMessage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the log level for the success message.
+        /// </summary>
+        LogLevel SuccessLogLevel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the failure message for the log action.
+        /// </summary>
+        string FailureMessage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the log level for the failure message.
+        /// </summary>
+        LogLevel FailureLogLevel { get; set; }
+    }
+}
---
+++ src/OnionCrafter.Service/Logging/LogAction.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace OnionCrafter.Service.Logging
+{
+    /// <summary>
+    /// Represents a log action.
+    /// </summary>
+    public class LogAction : ILogAction
+    {
+        /// <inheritdoc/>
+        public string SuccessMessage { get; set; }
+
+        /// <inheritdoc/>
+        public LogLevel SuccessLogLevel { get; set; }
+
+        /// <inheritdoc/>
+        public string FailureMessage { get; set; }
+
+        /// <inheritdoc/>
+        public LogLevel FailureLogLevel { get; set; }
+
+        /// <summary>
+        /// Default constructor for the LogAction class.
+        /// Initializes the properties with default values.
+        /// </summary>
+        public LogAction()
+        {
+            SuccessMessage = string.Empty;
+            SuccessLogLevel = LogLevel.Information;
+            FailureMessage = string.Empty;
+            FailureLogLevel = LogLevel.Error;
+        }
+    }
+}
