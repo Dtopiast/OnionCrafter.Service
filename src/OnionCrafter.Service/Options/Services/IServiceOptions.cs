fe6cd1706c225dfba68919132d440bc24fd4cd1b
dtopiast <dtopiast@protonmail.com>
2023-06-22 01:51:02 -0600

    Update of Services

---
+++ src/OnionCrafter.Service/Options/Services/IServiceOptions.cs
@@ -0,0 +1,16 @@
+﻿using OnionCrafter.Base.Commons;
+using OnionCrafter.Service.Utils;
+
+namespace OnionCrafter.Service.Options.Services
+{
+    /// <summary>
+    /// Interface for service options.
+    /// </summary>
+    public interface IServiceOptions : IBaseServiceOptions, IUseLogger, ISetName
+    {
+        /// <summary>
+        /// Gets or sets the service name.
+        /// </summary>
+        public new string? SetName { get; set; }
+    }
+}
